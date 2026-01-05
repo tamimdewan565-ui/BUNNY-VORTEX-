c63ba5c5ab76d6dc6b6ebd94a31eaed21082aade
tamimdewan565-ui <tamimdewan565@gmail.com>
2026-01-05 20:48:19 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -110,7 +110,7 @@ module.exports = {
         + `║ 💬 Chatbox : ${threadPrefix}\n`
         + `║ ➤ ${threadPrefix}help to see all available cmds 🥵\n`
         + "╚═══════════════╝",
-        attachment: await utils.getStreamFromURL("https://files.catbox.moe/ykk54z.jpg")
+        attachment: await utils.getStreamFromURL("https://files.catbox.moe/gyp97b.mp4")
       });
     }
   }
