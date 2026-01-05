795b8095da54da8fe2477889d243012c121e216e
tamimdewan565-ui <tamimdewan565@gmail.com>
2026-01-05 20:33:27 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -114,7 +114,7 @@ module.exports = {
       msg += `\n━━━━━━━━━━━━━━\n`;
       msg += `🔢 Total Commands: ${total}\n`;
       msg += `⚡ Prefix: ${prefix}\n`;
-      msg += `👑 Owner: Aphelion`;
+      msg += `👑 Owner: BUNNY VORTEX`;
 
       return message.reply(msg);
     }
